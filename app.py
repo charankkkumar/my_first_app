a8244559847617bb3d44fe14924e39dd5fa893f8
Indhumathy <indhumathy84@gmail.com>
2024-03-21 19:58:23 +0530

    first commit

--- app.py
+++ app.py
@@ -1,38 +1,47 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request#import flask library
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
+#initialize flask
+
+#route your webpage
+
 def visitors():
 
-    # Load current count
-    counter_read_file = open("count.txt", "r")
-    visitors_count = int(counter_read_file.read())
-    counter_read_file.close()
+	# Load current count
+	counter_read_file = open("count.txt", "r")
+	visitors_count = int(counter_read_file.read())
+	counter_read_file.close()
 
-    # Increment the count
-    visitors_count = visitors_count + 1
+	# Increment the count
+	visitors_count = visitors_count + 1
 
-    # Overwrite the count
-    counter_write_file = open("count.txt", "w")
-    counter_write_file.write(str(visitors_count))
-    counter_write_file.close()
+	# Overwrite the count
+	counter_write_file = open("count.txt", "w")
+	counter_write_file.write(str(visitors_count))
+	counter_write_file.close()
+	return render_template("index.html", count=visitors_count)
 
-    # Render HTML with count variable
-    return render_template("index.html", count=visitors_count)
+@app.route("/", methods=["POST"])
+def weather_stats():
+	# Load current count
+	counter_read_file = open("count.txt", "r")
+	visitors_count = int(counter_read_file.read())
+	counter_read_file.close()
 
-@app.route('/', methods=['POST'])
-def covid_stats():
-    # Load current count
-    counter_read_file = open("count.txt", "r")
-    visitors_count = int(counter_read_file.read())
-    counter_read_file.close()
+	latitude = request.form['Latitude']
+	longitude = request.form['Longitude']
 
-    text = request.form['text']
+	api_url = 'https://weather-l6tl.onrender.com/api/getCurrentWeather/'+latitude+'/'+longitude
 
-    corona_data = 'https://corona.dnsforfamily.com/graph.png?c='+text
-    print(corona_data)
-    return render_template("index.html", image=corona_data, count=visitors_count)
+	response = requests.get(api_url)
+	weather_data = response.json()
+	print(weather_data)
+	return render_template("index.html", weather = weather_data, count=visitors_count)
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
+
+#add code for executing flask
